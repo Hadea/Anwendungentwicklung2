092e0f6160f4d7cd66b5e7781ee0c1f646d7e769
Lutz <lambda2@gmx.at>
2020-12-15 11:51:10 +0100

    added image control

---
+++ Controls/pgeImage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Controls
+{
+    /// <summary>
+    /// Interaction logic for pgeImage.xaml
+    /// </summary>
+    public partial class pgeImage : Page
+    {
+        public pgeImage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVisibility_Click(object sender, RoutedEventArgs e)
+        {
+            imgVisibility.Visibility = (imgVisibility.Visibility) switch
+            {
+                Visibility.Visible => Visibility.Hidden,
+                Visibility.Hidden => Visibility.Collapsed,
+                _ => Visibility.Visible
+            };
+        }
+
+        private void btnScaling_Click(object sender, RoutedEventArgs e)
+        {
+            imgScaling.Stretch = (imgScaling.Stretch) switch
+            {
+                Stretch.None => Stretch.Uniform,
+                Stretch.Uniform => Stretch.UniformToFill,
+                Stretch.UniformToFill => Stretch.Fill,
+                _ => Stretch.None                
+            };
+        }
+    }
+}
