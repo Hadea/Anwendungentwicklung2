11b39d6429dd42547f7e5ffa7c56fef8275e25d0
Lutz <lambda2@gmx.at>
2021-01-12 10:27:42 +0100

    corrected and commented tests for List

--- ContainerTest/ListTests.cs
+++ ContainerTest/ListTests.cs
@@ -8,14 +8,16 @@ namespace ContainerTest
     public class ListTests
     {
         [TestMethod]
-        public void Createable()
+        public void CreateableStandard()
         {
+            // testet ob eine Liste mit standardkonstruktor erstellt werden kann
             Assert.IsNotNull(new List());
         }
 
         [TestMethod]
         public void IsEmptyAfterNew()
         {
+            // testet ob eine neue liste auch leer ist
             List testList = new();
             Assert.IsTrue(testList.IsEmpty());
         }
@@ -23,6 +25,7 @@ namespace ContainerTest
         [TestMethod]
         public void IsEmptyAfterAddAndRemove()
         {
+            // testet ob die liste nach einfügen und entfernen von einem wert wieder leer ist
             List testList = new();
             testList.Add(1);
             testList.Remove(0);
@@ -31,6 +34,7 @@ namespace ContainerTest
         [TestMethod]
         public void IsNotEmptyAfterAdd()
         {
+            // testet ob die liste nach einem einfügen eine Anzahl von einem element hat
             List testList = new();
             testList.Add(1);
             Assert.IsTrue(testList.Count == 1);
@@ -113,7 +117,7 @@ namespace ContainerTest
 
         }
         [TestMethod]
-        public void EqualAfterLoopAddAndPopWithParameter()
+        public void EqualAfterLoopAddAndAtWithParameter()
         {
             // testet ob der überladene construktor funkitoniert
             List testList = new(90000);
@@ -144,30 +148,31 @@ namespace ContainerTest
         }
 
         [TestMethod]
-        public void ContinuousAddAndPopOnMaxCapacity()
+        public void ContinuousAddRemoveAndAtOnMaxCapacity()
         {
             // testet ob nach mehrfachen einfügen und löschen das vergrössern gestartet wird und die werte trotzdem
             // korrekt hintereinander stehen
             List testList = new(4);
-            testList.Add(1);
-            testList.Add(2);
-            testList.Add(3);
-            testList.Add(4);
-            testList.Remove(1);
-            testList.Remove(0);
-            Assert.IsTrue(testList.At(0) == 3);
-            Assert.IsTrue(testList.At(1) == 4);
-            testList.Add(5);
-            testList.Add(6);
-            Assert.IsTrue(testList.Remove(0) == 3);
-            testList.Add(7);
-            testList.Add(8);
-            Assert.IsTrue(testList.Remove(0) == 4);
+            testList.Add(1); // 1
+            testList.Add(2); // 1 2
+            testList.Add(3); // 1 2 3
+            testList.Add(4); // 1 2 3 4
+            testList.Remove(1); // 1 3 4
+            testList.Remove(0); // 3 4
+            Assert.IsTrue(testList.At(0) == 3); // 3 4
+            Assert.IsTrue(testList.At(1) == 4); // 3 4
+            testList.Add(5); // 3 4 5
+            testList.Add(6); // 3 4 5 6
+            Assert.IsTrue(testList.At(3) == 6); // 3 4 5 6
+            testList.Add(7); // 3 4 5 6 7  vergrösserung der Liste
+            testList.Add(8); // 3 4 5 6 7 8
+            Assert.IsTrue(testList.At(5) == 8);
         }
 
         [TestMethod]
-        public void ContinuousAddTiming()
+        public void ContinuousAddAndRemoveTiming()
         {
+            // testet die geschwindigkeit der grössenänderung des array
             List testList = new(5);
             DateTime testStart = DateTime.Now;
             for (int counter = 0; counter < 100000; counter++)
@@ -179,51 +184,38 @@ namespace ContainerTest
         }
 
         [TestMethod]
-        public void ZBonusResizeAutomated()
+        public void ResizeAutomated()
         {
+            // testet die automatische verkleinerung der Liste
             List testList = new(5);
             for (int counter = 0; counter < 20; counter++)
                 testList.Add(counter);
             for (int counter = 0; counter < 10; counter++)
-                Assert.IsTrue(testList.Remove(0) == counter);
+                testList.Remove(0);
             for (int counter = 20; counter < 40; counter++)
                 testList.Add(counter);
             for (int counter = 10; counter < 30; counter++)
-                Assert.IsTrue(testList.Remove(0) == counter);
+                testList.Remove(0);
             Assert.IsTrue(testList.Capacity <= 20);
         }
 
         [TestMethod]
-        public void ZBonusResizeManualMiddle()
+        public void ResizeManualMiddle()
         {
             List testList = new(10);
-            for (int counter = 0; counter < 8; counter++)
+            for (int counter = 0; counter < 8; counter++) // 0 1 2 3 4 5 6 7
                 testList.Add(counter);
-            for (int counter = 0; counter < 4; counter++)
-                Assert.IsTrue(testList.Remove(0) == counter);
+            for (int counter = 0; counter < 4; counter++)// 4 5 6 7
+                testList.Remove(0);
             testList.Capacity = 5;
             for (int counter = 4; counter < 8; counter++)
-                Assert.IsTrue(testList.Remove(0) == counter);
+                Assert.IsTrue(testList.At(counter - 4) == counter);
         }
 
         [TestMethod]
-        public void ZBonusResizeManualEnd()
+        public void ResizeTooSmall()
         {
-            List testList = new(10);
-            for (int counter = 0; counter < 10; counter++)
-                testList.Add(counter);
-            for (int counter = 0; counter < 8; counter++)
-                Assert.IsTrue(testList.Remove(0) == counter);
-            for (int counter = 10; counter < 12; counter++)
-                testList.Add(counter);
-            testList.Capacity = 5;
-            for (int counter = 8; counter < 12; counter++)
-                Assert.IsTrue(testList.Remove(0) == counter);
-        }
-
-        [TestMethod]
-        public void ZBonusResizeTooSmall()
-        {
+            // testet ob ein zu starkes verkleinern eine fehlermeldung zeigt
             List testList = new(10);
             for (int counter = 0; counter < 8; counter++)
                 testList.Add(counter);
@@ -231,15 +223,17 @@ namespace ContainerTest
         }
 
         [TestMethod]
-        public void ZBonusCapacityOutOfRange()
+        public void CapacityOutOfRange()
         {
+            // testet ob eine negative grösse der liste eine fehlermeldung erzeugt
             List testList = new();
             Assert.ThrowsException<ArgumentOutOfRangeException>(() => testList.Capacity = -1);
         }
 
         [TestMethod]
-        public void ZBonusCapacityNeverBelowInitialization()
+        public void CapacityNeverBelowInitialization()
         {
+            // testet ob die grösse der liste nie unter die initialisierungsgrösse fällt
             List testList = new(10);
             for (int counter = 0; counter < 30; counter++)
                 testList.Add(counter);
@@ -249,8 +243,9 @@ namespace ContainerTest
         }
 
         [TestMethod]
-        public void ZBonusCapacityMinimumOnUserDefinition()
+        public void CapacityMinimumOnUserDefinition()
         {
+            // testet ob die kapazität der liste nimals unter den benutzerdefinierten wert fällt
             List testList = new(10);
             for (int counter = 0; counter < 30; counter++)
                 testList.Add(counter);
@@ -264,16 +259,18 @@ namespace ContainerTest
             Assert.IsTrue(testList.Capacity == 5);
         }
         [TestMethod]
-        public void ZBonusDirectResize()
+        public void DirectResize()
         {
+            // Testet ob der benutzer die kapazität anpassen kann
             List testList = new(15);
             testList.Capacity = 7;
             Assert.IsTrue(testList.Capacity == 7);
         }
 
         [TestMethod]
-        public void ZBonusForEachDelegate()
+        public void ForEachDelegate()
         {
+            // testet ob ForEach alle elemente durchgeht und für jedes die übergebene methode (hier ein lambda) startet
             List testList = new(10);
             testList.Add(1);
             testList.Add(2);
