30b4b9b75f789f9372f5d389352cc13483cf76ff
Lutz <lambda2@gmx.at>
2021-01-18 12:26:13 +0100

    added command and propertybinding

---
+++ TicTacToe/FieldButtonCommands.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Input;
+
+namespace TicTacToe
+{
+    public static class FieldButtonCommands
+    {
+        public static readonly RoutedUICommand Select = new RoutedUICommand(
+            "FieldButton Selection",
+            "Select",
+            typeof(FieldButtonCommands));
+
+    }
+}
--- TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/MainWindow.xaml.cs
@@ -5,20 +5,61 @@ using System.Media;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Input;
 using TicTacToeLogic;
+using System.ComponentModel;
 
 namespace TicTacToe
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         readonly GameLogic logic;
         readonly Button[,] buttonArray; // enthält alle buttons damit wir schnell auf sie zugreifen können und auch foreach funktioniert
 
-        SoundPlayer soundFX;
-        MediaPlayer soundMusic;
+        readonly MediaPlayer soundMusic;
+
+        private string message;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Message
+        {
+            get { return message; }
+            set
+            {
+                if (message != value)
+                {
+                    message = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Message)));
+                }
+            }
+        }
+
+        private bool musicPlaying;
+
+        public bool Music
+        {
+            get { return musicPlaying; }
+            set
+            {
+                if (musicPlaying != value)
+                {
+                    musicPlaying = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Music)));
+                    if (musicPlaying)
+                        soundMusic.Play();
+                    else
+                    {
+                        soundMusic.Pause();
+                        soundMusic.Position = TimeSpan.Zero;
+                    }
+                }
+            }
+        }
+
 
         /// <summary>
         /// Standard Construktor, prepares a new game.
@@ -27,42 +68,24 @@ namespace TicTacToe
         {
             InitializeComponent();// läd die XAML datei und erstellt die hinterlegten komponenten. Code immer erst danach einfügen.
             logic = new(); // Die Spiellogik wird erstellt
+            DataContext = this;
 
             // alle buttons welche als Spielfeld verwendet werden werden zusätzlich noch in einem Array
             // abgelegt damit wir sie bequem mit schleifen und koordinaten ansprechen können
             buttonArray = new Button[3, 3];
-            buttonArray[0, 0] = btnField11;
-            buttonArray[0, 1] = btnField21;
-            buttonArray[0, 2] = btnField31;
-            buttonArray[1, 0] = btnField12;
-            buttonArray[1, 1] = btnField22;
-            buttonArray[1, 2] = btnField32;
-            buttonArray[2, 0] = btnField13;
-            buttonArray[2, 1] = btnField23;
-            buttonArray[2, 2] = btnField33;
-            lblMessage.Content = $"Spieler {(logic.GetCurrentPlayer() ? "X" : "O")} ist am Zug";
+            buttonArray[0, 0] = grdFieldButtons.Children[0] as Button;
+            buttonArray[0, 1] = grdFieldButtons.Children[1] as Button;
+            buttonArray[0, 2] = grdFieldButtons.Children[2] as Button;
+            buttonArray[1, 0] = grdFieldButtons.Children[3] as Button;
+            buttonArray[1, 1] = grdFieldButtons.Children[4] as Button;
+            buttonArray[1, 2] = grdFieldButtons.Children[5] as Button;
+            buttonArray[2, 0] = grdFieldButtons.Children[6] as Button;
+            buttonArray[2, 1] = grdFieldButtons.Children[7] as Button;
+            buttonArray[2, 2] = grdFieldButtons.Children[8] as Button;
+            Message = $"Spieler {(logic.GetCurrentPlayer() ? "X" : "O")} ist am Zug";
             soundMusic = new();
-            soundMusic.Open(new Uri( Directory.GetParent(Environment.CommandLine) + @"\ShaolinDub-HarpDubz.mp3", UriKind.Absolute));
+            soundMusic.Open(new Uri(Directory.GetParent(Environment.CommandLine) + @"\ShaolinDub-HarpDubz.mp3", UriKind.Absolute));
 
-        }
-
-        /// <summary>
-        /// Resets the logic and the playing field
-        /// </summary>
-        /// <param name="sender">Reset Button reference, currently unused</param>
-        /// <param name="e">Arguments, currently unused</param>
-        private void btnReset_Click(object sender, RoutedEventArgs e)
-        {
-            logic.Reset();// logik wird wieder auf den anfangszustand gesetzt
-
-            // alle button werden reaktiviert und der text entfernt
-            foreach (var item in buttonArray)
-            {
-                item.Content = "";
-                item.IsEnabled = true;
-            }
-
-            lblMessage.Content = $"Spieler {(logic.GetCurrentPlayer() ? "X" : "O")} ist am Zug";
         }
 
         /// <summary>
@@ -78,6 +101,7 @@ namespace TicTacToe
                 for (int x = 0; x < buttonArray.GetLength(1); x++)
                 {
                     buttonArray[y, x].Content = board[y, x] switch { FieldState.X => "X", FieldState.O => "O", _ => "" };
+                    //buttonArray[y, x].IsEnabled = board[y, x] switch { FieldState.X => false, FieldState.O => false, _ => true };
                 }
             }
         }
@@ -87,59 +111,71 @@ namespace TicTacToe
         /// </summary>
         /// <param name="Result">Result of Turn()</param>
         /// <returns>If a button should be active</returns>
-        private bool evaluateTurn(TurnResult Result)
+        private void evaluateTurn(TurnResult Result)
         {
             switch (Result) // wertet den Parameter Result aus
             {
                 case TurnResult.Valid: // wenn der zug gültig war
                     redrawField(); // spielfeld aktualisieren
-                    return false; // rückgabe das der Button inaktiv werden soll
+                    Message = $"Spieler {(logic.GetCurrentPlayer() ? "X" : "O")} ist am Zug";
+                    break;
                 case TurnResult.Win: // wenn der zug zum sieg geführt hat
                     redrawField(); // spielfeld aktualisieren
-                    lblMessage.Content = $"Sieg! Spieler {(logic.GetCurrentPlayer() ? "X" : "O")} hat gewonnen"; // nachricht ausgeben das der aktuelle spieler gewonnen hat
+                    Message = $"Sieg! Spieler {(logic.GetCurrentPlayer() ? "X" : "O")} hat gewonnen"; // nachricht ausgeben das der aktuelle spieler gewonnen hat
 
                     // alle button deaktivieren, da das Spiel vorbei ist
                     foreach (var item in buttonArray)
-                    {
                         item.IsEnabled = false;
-                    }
-                    return false;
+                    break;
                 case TurnResult.Tie: // wenn der zug zu einem unentschieden geführt hat
                     redrawField(); // spielfeld aktualisieren
-                    lblMessage.Content = "Unentschieden!"; // Spieler benachrichtigen
-                    return false; // rückgabe das der button der zum unentschieden geführt hat deaktiviert werden soll
+                    Message = "Unentschieden!"; // Spieler benachrichtigen
+                    break;
                 default: // wenn der zug ungültig war (sollte nicht auftreten)
-                    lblMessage.Content = "Das war nix";
-                    return true; // button darf aktiv bleiben
+                    Message = "Das war nix";
+                    break;
             }
         }
 
-        // Methoden für die 9 Spielfeld-Buttons
-        // Jeder button löst mit seinen koordinaten einen Zug aus, und reicht das
-        // ergebnis des zuges an das evaluateTurn weiter
-        // evaluateTurn gibt als Rückgabe ob der button sich deaktivieren soll oder nicht
-        private void btnField11_Click(object sender, RoutedEventArgs e) => (sender as Button).IsEnabled = evaluateTurn(logic.Turn(new PointB(0, 0)));
-        private void btnField21_Click(object sender, RoutedEventArgs e) => (sender as Button).IsEnabled = evaluateTurn(logic.Turn(new PointB(1, 0)));
-        private void btnField31_Click(object sender, RoutedEventArgs e) => (sender as Button).IsEnabled = evaluateTurn(logic.Turn(new PointB(2, 0)));
-        private void btnField12_Click(object sender, RoutedEventArgs e) => (sender as Button).IsEnabled = evaluateTurn(logic.Turn(new PointB(0, 1)));
-        private void btnField22_Click(object sender, RoutedEventArgs e) => (sender as Button).IsEnabled = evaluateTurn(logic.Turn(new PointB(1, 1)));
-        private void btnField32_Click(object sender, RoutedEventArgs e) => (sender as Button).IsEnabled = evaluateTurn(logic.Turn(new PointB(2, 1)));
-        private void btnField13_Click(object sender, RoutedEventArgs e) => (sender as Button).IsEnabled = evaluateTurn(logic.Turn(new PointB(0, 2)));
-        private void btnField23_Click(object sender, RoutedEventArgs e) => (sender as Button).IsEnabled = evaluateTurn(logic.Turn(new PointB(1, 2)));
-        private void btnField33_Click(object sender, RoutedEventArgs e) => (sender as Button).IsEnabled = evaluateTurn(logic.Turn(new PointB(2, 2)));
 
-        private void cbPlay_Click(object sender, RoutedEventArgs e)
+        private void New_Command(object sender, ExecutedRoutedEventArgs e)
         {
-            var cb = (sender as CheckBox);
-            if (cb.IsChecked.Value)
+            logic.Reset();// logik wird wieder auf den anfangszustand gesetzt
+
+            // alle button werden reaktiviert und der text entfernt
+            foreach (var item in buttonArray)
             {
-                soundMusic.Play();
+                item.Content = "";
+                //item.IsEnabled = true;
+            }
+
+            Message = $"Spieler {(logic.GetCurrentPlayer() ? "X" : "O")} ist am Zug";
+        }
+
+        private void Close_Command(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Select_Command(object sender, ExecutedRoutedEventArgs e)
+        {
+            object[] parameters = e.Parameter as object[];
+            byte X = (byte)parameters[0];
+            byte Y = (byte)parameters[1];
+            evaluateTurn(logic.Turn(new PointB(X, Y)));
+        }
+
+        private void Select_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (e.Parameter != null)
+            {
+                object[] parameters = e.Parameter as object[];
+                byte X = (byte)parameters[0];
+                byte Y = (byte)parameters[1];
+                e.CanExecute = logic.GetBoard()[Y, X] switch { FieldState.O => false, FieldState.X => false, _ => true };
             }
             else
-            {
-                soundMusic.Pause();
-            }
-
+                e.CanExecute = false;
         }
     }
 }
