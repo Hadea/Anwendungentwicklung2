cf8997e6808b835bb0948578b87e5379b0e76163
Lutz <lambda2@gmx.at>
2021-01-20 11:15:32 +0100

    Added Login project with multibinding and converter

---
+++ Login/LoginViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.Windows.Input;
+
+namespace Login
+{
+    public class LoginViewModel : INotifyPropertyChanged
+    {
+        public ICommand UserLogin { get; init; }
+
+        public LoginViewModel()
+        {
+            UserLogin = new UserLoginCommand(this);
+        }
+
+        public string UserLoginResult
+        {
+            get { return result; }
+            set
+            {
+                if (result != value)
+                {
+                    result = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(UserLoginResult)));
+                }
+            }
+        }
+        private string result;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ Login/UserLoginCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Login
+{
+    internal class UserLoginCommand : ICommand
+    {
+        private readonly LoginViewModel parent;
+
+        public UserLoginCommand(LoginViewModel loginViewModel)
+        {
+            parent = loginViewModel;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            object[] paramConverted = parameter as object[];
+            if (paramConverted[0] as string == "Admin" && (paramConverted[1] as PasswordBox).Password == "123456")
+                parent.UserLoginResult = "Willkommen " + paramConverted[0];
+            else
+                parent.UserLoginResult = "Nope!";
+        }
+    }
+}
