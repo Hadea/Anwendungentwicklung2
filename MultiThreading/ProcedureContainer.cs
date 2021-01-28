1c64116312a0f52e669243d9ac23721498e9e07c
Lutz <lambda2@gmx.at>
2021-01-28 11:15:39 +0100

    added threading example

---
+++ MultiThreading/ProcedureContainer.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+
+namespace MultiThreading
+{
+    class ProcedureContainer
+    {
+        public static string Wait5SecForString()
+        {
+            Thread.Sleep(5000);
+            return "done";
+        }
+    }
+}
