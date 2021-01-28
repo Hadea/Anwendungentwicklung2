2827fe8f6a1de3534feee34e95c793cefe22569b
Lutz <lambda2@gmx.at>
2021-01-28 15:25:13 +0100

    added console example for threading

---
+++ ThreadingConsole/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ThreadingConsole
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine($"Willkommen zum Threading-Spielplatz. Sie haben {Environment.ProcessorCount} Prozessoren. Loslogen mit Enter");
+            Console.ReadLine();
+
+            var t = new Task<int>[4];
+            for (int counter = 0; counter < t.Length; counter++)
+                t[counter] = new Task<int>(() => { return countUp("T" + counter.ToString()); });
+
+            foreach (var item in t) item.Start();
+
+            DateTime startTime = DateTime.Now;
+            while ((DateTime.Now - startTime).TotalSeconds < 2)
+            {
+                Task.Delay(250).Wait();
+                Console.WriteLine("Aktueller Zählerstand: {0}", counter);
+            }
+
+            Task.WaitAll(t);
+
+            int summeThreads = 0;
+            foreach (var item in t) summeThreads += item.Result;
+
+            Console.WriteLine("letzter Zählerstand: {0}, summe der Threads: {1}", counter, summeThreads);
+            Console.WriteLine("Fertig");
+            Console.ReadLine();
+        }
+
+        static int counter = 0;
+
+        static int countUp(string name)
+        {
+            int eigenerCounter = 0;
+            DateTime startTime = DateTime.Now;
+            while ((DateTime.Now - startTime).TotalSeconds < 5)
+            {
+                eigenerCounter += 1;
+                counter += 1;
+            }
+            Console.WriteLine($"Thread: {name}, intern: {eigenerCounter}");
+            return eigenerCounter;
+        }
+    }
+}
