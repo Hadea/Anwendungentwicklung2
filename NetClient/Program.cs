c606735591dc233fdcf61b4aa9dc93a617ea0158
Lutz <lambda2@gmx.at>
2021-02-08 13:19:22 +0100

    added example for TCP network

---
+++ NetClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace NetClient
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hallo, ich bin der Client");
+
+            TcpClient connection = new();
+            connection.Connect("127.0.0.1", 1337);
+            if (connection.Connected)
+            {
+                Console.WriteLine("Jup, sind verbunden"); 
+            }
+            else
+            {
+                Console.WriteLine("war nix");
+                Console.ReadLine();
+                return;
+            }
+
+            Console.Write("Bitte nachricht eingeben: ");
+            string message = Console.ReadLine();
+            byte[] data = Encoding.ASCII.GetBytes(message);
+            var dataStream = connection.GetStream();
+            dataStream.Write(data, 0, data.Length);
+            dataStream.Flush();
+            dataStream.Close();
+            connection.Close();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ NetServer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+
+namespace NetServer
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hallo, ich bin der Server");
+            TcpListener listener = new(IPAddress.Any, 1337);
+            Console.WriteLine("port reserviert, warte auf verbindung");
+            listener.Start();
+
+            TcpClient connection = listener.AcceptTcpClient();
+            Console.WriteLine("Client verbunden");
+
+            var stream = connection.GetStream();
+            int recievedBytes;
+            byte[] data = new byte[1024];
+            while ((recievedBytes = stream.Read(data, 0 , data.Length)) != 0)
+            {
+                string message = Encoding.ASCII.GetString(data);
+                Console.WriteLine("Nachricht vom Client : {0}",message);
+            }
+
+            stream.Close();
+            connection.Close();
+            listener.Stop();
+            Console.ReadLine();
+        }
+    }
+}
