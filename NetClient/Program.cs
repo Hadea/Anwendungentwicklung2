3c6ea93009e76c0a4d63348ecb3e4a4153fe261c
Lutz <lambda2@gmx.at>
2021-02-09 10:42:15 +0100

    added comments

--- NetClient/Program.cs
+++ NetClient/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Net.Sockets;
-using System.Text;
+using System.Net.Sockets; // TCPClient
+using System.Text; // Encoding
 
 namespace NetClient
 {
@@ -12,6 +12,17 @@ namespace NetClient
             Console.WriteLine("Startklar?");
             Console.ReadLine();
 
+            /* Der TCPClient verwaltet eine Verbindung zu einem Computer
+             * über TCP/IP.
+             * Mit dem Connect befehl stellen wir die Verbindung her
+             * dabei geben wir an wo der Computer zu finden ist (IP)
+             * und mit welchem Dienst wir verbunden werden wollen (Port)
+             * 
+             * Sollte es dabei zu Problemen kommen (Firewall, Falsche IP,...)
+             * wird eine Exeption geworfen welche details enthält.
+             * 
+             * In Connected steht ob die Verbindung noch immer besteht
+             */
             TcpClient connection = new();
             connection.Connect("127.0.0.1", 1337);
             if (connection.Connected)
@@ -25,19 +36,45 @@ namespace NetClient
                 return;
             }
 
+            /* Die Kommunikation wird über einen Stream gehandhabt welcher
+             * Read und Write befehle über den TCPClient mit dem anderen
+             * Computer abwickelt.
+             */
             var dataStream = connection.GetStream();
             while (true)
             {
                 Console.Write("Bitte nachricht eingeben: ");
                 string message = Console.ReadLine();
 
+                // solange der nutzer nicht stop tippt wird die eingabe gesendet
                 if (message == "stop")
                 {
                     break;
                 }
+
+                /* Die Übertragung zum anderen Computer wird mit byte-Arrays
+                 * bewerkstelligt. Dazu müssen wir den String erst Konvertieren
+                 * (serialisieren).
+                 * C# liefert bereits einige tools dafür mit. In unserem Fall
+                 * genügt GetBytes.
+                 * 
+                 * Der Convertierte String wird dann mit Write übertragen wobei
+                 * immer auch der Ausschnitt des Arrays angegeben wird welcher
+                 * die Nachricht enthält. Dies ist besonders nützlich wenn die
+                 * zu übertragende Nachricht sehr gross ist und in einzelnen
+                 * schritten übertragen werden muss.
+                 */
                 byte[] data = Encoding.ASCII.GetBytes(message);
                 dataStream.Write(data, 0, data.Length);
             }
+
+            /* Wenn der Nutzer stop getippt hat verlassen wir die Schleife
+             * und fahren unsere Seite der Verbindung herunter.
+             * Die Reihenfolge ist dabei wichtig.
+             * Durch den TCPClient.Close() befehl wird dem Server eine
+             * Nachricht übermittelt das die Verbindung regulär beendet
+             * wird.
+             */
             Console.WriteLine("Client ist dann jetzt wech");
             dataStream.Flush();
             dataStream.Close();
--- NetServer/Program.cs
+++ NetServer/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Net;
-using System.Text;
-using System.Net.Sockets;
+using System.Net; // IPAddress
+using System.Text; // Encoding
+using System.Net.Sockets; // TcpListener und TCPClient
 
 namespace NetServer
 {
@@ -10,26 +10,70 @@ namespace NetServer
         static void Main()
         {
             Console.WriteLine("Hallo, ich bin der Server");
+
+            // startet den TcpListener welcher auf allen Netzwerkadressen
+            // auf diesem Computer auf einen Verbindungsversuch wartet
+            // dieser Verbindungsversuch muss über Port 1337 geschehen
+
+            // Der Port darf dabei nicht von einer anderen Anwendung belegt
+            // sein und sollte idealerweise nicht unter 1024 sein (reserviert)
             TcpListener listener = new(IPAddress.Any, 1337);
-            Console.WriteLine("port reserviert, warte auf verbindung");
             listener.Start();
 
+            Console.WriteLine("port reserviert, warte auf verbindung");
+
+            // AcceptTcpClient wartet bis ein TCPClient eine Verbindung
+            // mit diesem Computer aufbaut und erstellt einen TCPClient
+            // auf diesem Server
+            // Das TCPClient pärchen repräsentiert die Verbindung durch
+            // die nun daten ausgetauscht werden können
             TcpClient connection = listener.AcceptTcpClient();
             Console.WriteLine("Client verbunden");
 
+            // da wir keine weiteren Verbindungsverusche akzeptieren wollen
+            // fahren wir den Listener wieder herunter. Weitere 
+            // Verbindungsversuche werden jetzt aktiv abgelehnt
+            listener.Stop();
+            // Der Datenaustausch findet über einen Stream statt welcher
+            // mit read und write die daten über das netzwerk schickt und
+            // empfängt
+            // Da wir als empfänger nicht wissen wie gross die Nachricht
+            // des Senders ist erstellen wir ein Array das gross genug
+            // für jede nachricht ist und merken uns wieviel von dem Array
+            // benutzt wurde
             var stream = connection.GetStream();
             int recievedBytes;
             byte[] data = new byte[1024];
+
+            /* Read wartet auf eine Nachricht und lagert sie in einem Byte
+             * Array. Damit Read nicht zu viel auf einmal speichert geben
+             * wir die maximale byteanzahl mit an. In unserem fall soll das
+             * Array ab stelle 0 für maximal 1024 byte (length) beschrieben
+             * werden.
+             * Read gibt einen Integer zurück welcher mehrere bedeutungen hat.
+             *    grösser 0 : nachricht empfangen und die anzahl der empfangenen bytes
+             *    exakt 0: der TCPClient auf der anderen seite hat die verbindung beendet
+             *    kleiner 0: es gab einen Netzwerkfehler und die Verbindung ist weg
+             * Die while Schleife läuft demnach bis der andere die Verbindung trennt
+             */
             while ((recievedBytes = stream.Read(data, 0 , data.Length)) != 0)
             {
+                // die empfangene nachricht ist ein Bytearray und muss wieder in
+                // das original übersetzt werden (deserialisierung)
                 string message = Encoding.ASCII.GetString(data,0,recievedBytes);
-
                 Console.WriteLine("Nachricht vom Client : {0}",message);
             }
+
+            /* Der Client hat die Verbindung getrennt sodass wir aus der while schleife
+             * raus sind und können nun selbst unser Netzwerk herunterfahren
+             * Wichtig ist dabei die Reihenfolge
+             * Wie bei Datenbanken könnten wir das auch über ein using lösen,
+             * erfahrungsgemäss sind die methoden aber an so unterschiedlichen
+             * stellen im code das es nicht mehr mit einem using geht
+             */
             Console.WriteLine("Server ist wech");
             stream.Close();
             connection.Close();
-            listener.Stop();
             Console.ReadLine();
         }
     }
