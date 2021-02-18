57674a5e158964c5bad795d105b7ea0b6d3bd3b1
Lutz <lambda2@gmx.at>
2021-02-18 14:00:30 +0100

    added project for delegates, touples and lambdas

---
+++ Delegates/Logic.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    // muss exakt übereinstimmen
+    public delegate void KeinParameterKeineRueckgabe(); // macht das gleiche wie das mitgelieferte  Action
+    public delegate void EinParameterKeineRueckgabe(string para); //macht das gleiche wie das mitgelieferte  Action<string>
+    public delegate byte ZweiParameterEineRueckgabe(string paraA, bool paraB); //macht das gleiche wie das mitgelieferte  Func<string, bool,byte>
+
+
+    class Logic
+    {
+
+        public KeinParameterKeineRueckgabe LogicWillBescheidGeben;
+        public EinParameterKeineRueckgabe LogicWillTextWeitergeben;
+
+        public void AufrufVomUI()
+        {
+            Console.WriteLine("LG> UI hat gerde in der Logic eine Funktion gestartet");
+
+            if (LogicWillBescheidGeben != null && LogicWillTextWeitergeben != null)
+            {
+                Console.WriteLine("LG> Starte alle eingehängten Methoden ohne parameter und ohne rückgabe");
+                LogicWillBescheidGeben?.Invoke();
+
+                Console.WriteLine("LG> Starte alle eingehängten Methoden mit string parameter und ohne rückgabe");
+                LogicWillTextWeitergeben("Wichtige Nachricht der Logic");
+            }
+        }
+
+        public (int ,double) FunktionMitZweiRueckgaben()
+        {
+            int a = 1;
+            double b = 0.3;
+            return (a,b);
+        }
+
+        public void FunktionMitOutParameter(string Wort, out int Laenge)
+        {
+            Laenge = Wort.Length;
+        }
+
+    }
+
+ 
+
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Microsoft hat gepennt")]
+        static void Main()
+        {
+            Console.WriteLine("UI> Delegaten-Testprogramm");
+
+            Logic l = new();
+            l.LogicWillBescheidGeben = wennLogicWasZuSagenHat;
+            l.LogicWillTextWeitergeben = ausgabeWichtigerNachricht;
+
+            Console.WriteLine("UI> Starte eine Methode in der Logic");
+            l.AufrufVomUI();
+
+            Console.WriteLine("UI> Sind wieder in der Main und der erste Aufruf ist fertig");
+
+            l.LogicWillBescheidGeben += ausgabeWennLogicWasSagt;
+            l.AufrufVomUI();
+
+            Console.WriteLine("UI> Delegates und Lambdas");
+
+            l.LogicWillTextWeitergeben += (m) =>Console.WriteLine(m);
+
+
+            Console.WriteLine("UI> Spass mit Touples");
+
+            int gamma = 1;
+            
+            (gamma, _ )= l.FunktionMitZweiRueckgaben();
+
+            int meineZahl;
+            l.FunktionMitOutParameter("Hallo Welt", out meineZahl);
+            Console.WriteLine($"UI> Der Satz Hallo Welt hat {meineZahl} Zeichen");
+
+            Console.ReadLine();// damit das fenster offen bleibt
+        }
+
+        static void wennLogicWasZuSagenHat()
+        {
+            Console.WriteLine("UI> Logic hat eine Methode wennLogicWasZuSagenHat im UI gestartet");
+        }
+        static void ausgabeWennLogicWasSagt()
+        {
+            Console.WriteLine("UI> Logic hat eine Methode ausgabeWennLogicWasSagt im UI gestartet");
+        }
+
+        static void ausgabeWichtigerNachricht(string Nachricht)
+        {
+            Console.WriteLine("UI> Die nachricht von der Logic lautet: " + Nachricht);
+        }
+    }
+}
